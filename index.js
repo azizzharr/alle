1f790d2a02c6ad444213dc047de56ba41b8d274e
azizzharr <ivanhhd872@gmail.com>
2020-09-05 18:32:43 +0600

    Hausuafgabe von 9

---
+++ index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Blog = () =>{
+    return (
+        const div = {
+            border:'1px solid black',
+            margin: '2% auto',
+            width: '25%',
+            height: '550px'
+
+        }
+        const h2 = {
+            color:'red',
+            fontSize:'65px'
+
+        }
+        const alle ={
+            fontSize:'35px'
+        }
+
+    <div style={div} className={container}>
+        <h2 style={h2} className={form-group}>
+            Sing in
+        </h2>
+        Username
+        <input name="text" placeholder="Username" className={form-control alle form-group}>
+            Password
+            <input name="password" placeholder="Password" className={form-control alle form-group}>
+                <button type="button" className={btn btn-danger form-group alle}>Login</button>
+
+
+    </div>
+    )
+}
+ReactDom.render(<Blog/>, document.getElementById('root'))
